c34c36112b9eff12231ab61abaf07b6ca31a7486
Georgette Class-Peters <georgycp@udel.edu>
2020-09-07 17:47:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,22 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
   
  
 
